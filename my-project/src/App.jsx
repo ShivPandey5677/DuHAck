9f1421398038b67731e56db241187bffc8ea64bf
Prakriti Yadav <prakritiyadav2004@gmail.com>
2024-02-24 12:20:25 +0530

    hemlo

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
 
   <div  className="bg-slate-900">
+    <h2>h</h2>
     <Navbar/>
   </div>
 
--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -2,10 +2,61 @@ import React from 'react'
 
 
 const Navbar = () => {
+    const content =<>
+    <div className="">
+      <ul>
+        <a href=""><li>Home</li></a>
+
+        <a href=""><li>About</li></a>
+
+        <a href=""><li>LogIn</li></a>
+
+        <a href=""><li>SignUp</li></a>
+
+        <a href=""><li>Psychaitrist</li></a>
+
+        <a href=""><li>Contact</li></a>
+
+          
+      </ul>
+    </div></>
   return (
-    <div>
-      
-    </div>
+    <nav>
+        <div>
+            <div>
+                <span>LOGO</span>
+            </div>
+            <div>
+                <div>
+                    <ul>
+                    <a href=""><li>Home</li></a>
+                    </ul>
+
+                    <ul>
+                    <a href=""><li>About</li></a>
+                    </ul>
+
+                    <ul>
+                    <a href=""><li>LogIn</li></a>
+                    </ul>
+
+                    <ul>
+                    <a href=""><li>SignUp</li></a>
+                    </ul>
+
+                    <ul>
+                    <a href=""><li>Psychaitrist</li></a>
+                    </ul>
+
+                    <ul>
+                    <a href=""><li>Contact</li></a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </nav>
+    
   )
 }
 
